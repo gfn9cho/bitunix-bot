88ff2b774aa1a010136bb611a38ad2bb03c483b0
prahba.rec@gmail.com <YDiavel@1>
2025-05-05 22:18:00 -0500

    Added Websocket invoker

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from modules.logger_config import logger, trade_logger, error_logger, reversal_l
 from modules.config import API_KEY, API_SECRET, BASE_URL, POSITION_SIZE, LEVERAGE, MAX_DAILY_LOSS
 from modules.utils import parse_signal, calculate_zone_entries, calculate_quantities, update_loss, get_today_loss
 from modules.websocket_handler import start_websocket_listener
+from threading import Thread
 import os
 import hmac
 import hashlib
@@ -83,6 +84,14 @@ def simulate_tp():
 @app.route('/webhook/<symbol>', methods=['POST'])
 def webhook(symbol):
     return webhook_handler(symbol)
+
+def start_ws_thread():
+    thread = Thread(target=start_websocket_listener, daemon=True)
+    thread.start()
+    logger.info("WebSocket listener started in background thread.")
+
+start_ws_thread()
+
 
 if __name__ == '__main__':
     ws_thread = threading.Thread(target=start_websocket_listener, daemon=True)
