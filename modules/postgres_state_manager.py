8adc49825d51424cb0d668fc0923677f6cdd658b
prahba.rec@gmail.com <YDiavel@1>
2025-05-12 16:44:40 -0500

    Prevent false signal

--- modules/postgres_state_manager.py
+++ modules/postgres_state_manager.py
@@ -106,9 +106,9 @@ def update_position_state(symbol, direction, position_id, updated_fields: dict):
                 """, [symbol, direction, position_id] + values)
             if position_id is None:
                 cur.execute(f"""
-                        INSERT INTO position_state (symbol, direction, {', '.join(columns)})
+                        INSERT INTO position_state (symbol, direction, position_id, {', '.join(columns)})
                         VALUES (%s, %s, {placeholders})
-                        ON CONFLICT (symbol, direction ) DO UPDATE SET {set_clause}
+                        ON CONFLICT (symbol, direction, '' ) DO UPDATE SET {set_clause}
                     """, [symbol, direction] + values)
 
             conn.commit()
