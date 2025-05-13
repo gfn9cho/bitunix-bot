ec20157f161379460d27b13c78eb4806855d417b
prahba.rec@gmail.com <YDiavel@1>
2025-05-13 13:19:00 -0500

    Added redis url

--- modules/redis_client.py
+++ modules/redis_client.py
@@ -1,9 +1,17 @@
 # redis_client.py
 import redis
+import os
+from urllib.parse import urlparse
 
+redis_url = os.getenv("REDIS_URL")  # Render injects this automatically
+if not redis_url:
+    raise ValueError("REDIS_URL not set. Make sure Redis add-on is configured.")
+
+url = urlparse(redis_url)
 r = redis.Redis(
-    host='localhost',
-    port=6379,
+    host=url.hostname,
+    port=url.port,
+    password=url.password,
     db=0,
-    decode_responses=True  # optional, makes values return as str
+    decode_responses=True
 )
