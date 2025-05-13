73ba1ea8dd8b7523cbf22aa25b71156d3d55a67e
prahba.rec@gmail.com <YDiavel@1>
2025-05-13 15:27:32 -0500

    Redis url fix

--- modules/redis_client.py
+++ modules/redis_client.py
@@ -3,7 +3,7 @@ import redis
 import os
 from urllib.parse import urlparse
 
-redis_url = os.getenv("REDIS_URL")  # Render injects this automatically
+redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")  # Render injects this automatically
 if not redis_url:
     raise ValueError("REDIS_URL not set. Make sure Redis add-on is configured.")
 
@@ -11,7 +11,7 @@ url = urlparse(redis_url)
 r = redis.Redis(
     host=url.hostname,
     port=url.port,
-    password=url.password,
-    db=0,
-    decode_responses=True
+    db=0,  # default
+    decode_responses=True,
+    password=url.password or None  # handle no-password case
 )
